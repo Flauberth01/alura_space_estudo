2ddd2f7122a6c48d7e0d0e5ab6aa48b5ce0e1c3b
Flauberth01 <flauberth12@gmail.com>
2023-07-19 10:10:11 -0500

    Feat: adicionando novos usuarios, logica de cadastro e logica de login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,11 +5,70 @@ class LoginForms(forms.Form):
     nome_login = forms.CharField(
         label="Nome de Login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex:.. Flauberth Brito"
+            }
+        )
     )
     senha = forms.CharField(
-        label="senha",
+        label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite a sua senha"
+            }
+        )
+    )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex:.. Flauberth Brito"
+            }
+        )
+    )
+    
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex:.. flauberthbrito@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirme sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
     )
