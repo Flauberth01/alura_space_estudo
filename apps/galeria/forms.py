f83cb34b086ce3992555d126e4aa0c825f63a71c
Flauberth01 <flauberth12@gmail.com>
2023-07-24 16:10:02 -0500

    Feat: Adicionando funcionalidade de Deleção, edição e filtro

--- apps/galeria/forms.py
+++ apps/galeria/forms.py
@@ -6,6 +6,11 @@ class FotografiaForms(forms.ModelForm):
     class Meta:
         model = Fotografia
         exclude = ['publicada',]
+        labels = {
+            'descricao': 'Descrição',
+            'data_fotografia': 'Data de registro',
+            'usuario': 'Usuário',
+        }
 
         widgets = {
             'nome': forms.TextInput(attrs={'class':'form-control'}),
@@ -14,11 +19,11 @@ class FotografiaForms(forms.ModelForm):
             'descricao': forms.Textarea(attrs={'class':'form-control'}),
             'foto': forms.FileInput(attrs={'class':'form-control'}),
             'data_fotografia': forms.DateInput(
-                format = '%d/%m%Y', 
+                format = '%d/%m/%Y', 
                 attrs={
                     'type':'date',
                     'class':'form-control'
                 }
             ),
-            'usuario': forms.FileInput(attrs={'class':'form-control'}),
+            'usuario': forms.Select(attrs={'class':'form-control'}),
         }
